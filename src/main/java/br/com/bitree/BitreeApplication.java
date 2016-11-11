553f552e5a281a42f3413da4626d595ef2447295
israeleriston <israeleriston@bitbucket.org>
2016-11-11 18:37:03 -0500

    first commit the project

---
+++ src/main/java/br/com/bitree/BitreeApplication.java
@@ -0,0 +1,12 @@
+package br.com.bitree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BitreeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BitreeApplication.class, args);
+	}
+}
